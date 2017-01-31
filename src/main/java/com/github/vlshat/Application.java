f2d05509119811ca0e80b887a0eaafdff2416e48
Vladislav Shatilenko <shatilenko.v@gmail.com>
2017-02-01 01:13:38 +0300

    initial commit

---
+++ src/main/java/com/github/vlshat/Application.java
@@ -0,0 +1,12 @@
+package com.github.vlshat;
+
+public class Application
+{
+	public static void main( String[] args )
+	{
+	}
+
+	public void say(){
+		System.out.println("a");
+	}
+}
---
+++ src/test/java/com/github/vlshat/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.github.vlshat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wladislaw on 01.02.17.
+ */
+public class ApplicationTest {
+    @Test
+    public void say() throws Exception {
+
+    }
+
+}
