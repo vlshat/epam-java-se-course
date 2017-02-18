d8aaa67d6484f01a1ea47c7533ff9f25963d72e8
Vladislav Shatilenko <shatilenko.v@gmail.com>
2017-02-18 13:08:40 +0300

    structure unit -> project

---
+++ unit01/src/main/java/com/github/vlshat/unit02/Sorter.java
@@ -0,0 +1,18 @@
+package com.github.vlshat.unit02;
+
+import java.util.Comparator;
+
+/**
+ * Created by wladislaw on 14.02.17.
+ */
+public class Sorter implements Comparator<String>{
+
+    public String[] sortDesc(String[] a){
+        return a;
+    }
+
+    @Override
+    public int compare(String o1, String o2) {
+        return 0;
+    }
+}
