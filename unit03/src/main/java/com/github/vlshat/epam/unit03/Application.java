7bccbf9c03be1933915a7c354729685562ad0eda
Vladislav Shatilenko <shatilenko.v@gmail.com>
2017-02-27 12:37:22 +0300

    unit03 project was created, task01 was started

---
+++ unit03/src/main/java/com/github/vlshat/epam/unit03/Application.java
@@ -0,0 +1,7 @@
+package com.github.vlshat.epam.unit03;
+
+/**
+ * Created by wladislaw on 27.02.17.
+ */
+public class Application {
+}
---
+++ unit03/src/main/java/com/github/vlshat/epam/unit03/task01/CrazyLogger.java
@@ -0,0 +1,8 @@
+package com.github.vlshat.epam.unit03.task01;
+
+/**
+ * Created by wladislaw on 27.02.17.
+ */
+public class CrazyLogger {
+
+}
