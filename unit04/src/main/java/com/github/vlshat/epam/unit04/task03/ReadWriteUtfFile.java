7e02649fd1687be48c825d534c8aba38df175a5c
Vladislav Shatilenko <shatilenko.v@gmail.com>
2017-03-05 16:59:08 +0300

    task03

---
+++ unit04/src/main/java/com/github/vlshat/epam/unit04/task03/ReadWriteUtfFile.java
@@ -0,0 +1,25 @@
+package com.github.vlshat.epam.unit04.task03;
+
+import java.io.*;
+
+/**
+ * Created by wladislaw on 05.03.17.
+ */
+public class ReadWriteUtfFile {
+
+    public static void main(String[] args) throws IOException {
+
+        InputStreamReader reader = new InputStreamReader(
+                        new FileInputStream("text.unit04result"), "UTF8");
+
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+                new FileOutputStream("test2.unit04result"), "UTF16");
+
+        while (reader.ready()){
+            outputStreamWriter.write(reader.read());
+        }
+
+        reader.close();
+        outputStreamWriter.close();
+    }
+}
