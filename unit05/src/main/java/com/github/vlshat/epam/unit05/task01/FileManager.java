5326fb8f4efa256bf183cf7a3d588c8ce753dfb7
Vladislav Shatilenko <shatilenko.v@gmail.com>
2017-03-08 17:52:15 +0300

    FileManager template

---
+++ unit05/src/main/java/com/github/vlshat/epam/unit05/task01/FileManager.java
@@ -0,0 +1,43 @@
+package com.github.vlshat.epam.unit05.task01;
+
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.SecureDirectoryStream;
+
+
+public class FileManager {
+
+    private String currentPath = "/";
+
+    public String pwd(){
+        return null;
+    }
+
+    public String cd(String dir){
+        return null;
+    }
+
+    public String cdB(){
+        return null;
+    }
+
+    public String ls(){
+        return null;
+    }
+
+    public String cat(String fileName){
+        return null;
+    }
+
+    public String edit(String fileName, String text){
+        return null;
+    }
+
+    public void touch(String fileName, String text){
+
+    }
+
+    public void delete(String fileName){
+
+    }
+}
