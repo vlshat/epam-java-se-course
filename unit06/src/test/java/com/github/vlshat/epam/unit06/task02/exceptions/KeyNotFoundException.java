41ae4e57b435a413195266b04adc6729dae6974f
Vladislav Shatilenko <shatilenko.v@gmail.com>
2017-03-25 12:36:13 +0300

    task02

---
+++ unit06/src/test/java/com/github/vlshat/epam/unit06/task02/exceptions/KeyNotFoundException.java
@@ -0,0 +1,10 @@
+package com.github.vlshat.epam.unit06.task02.exceptions;
+
+/**
+ * Created by vladislav on 13.03.17.
+ */
+public class KeyNotFoundException extends PropertyManagerException {
+    public KeyNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ unit06/src/test/java/com/github/vlshat/epam/unit06/task02/exceptions/PropertyNotLoadedException.java
@@ -0,0 +1,10 @@
+package com.github.vlshat.epam.unit06.task02.exceptions;
+
+/**
+ * Created by vladislav on 13.03.17.
+ */
+public class PropertyNotLoadedException extends PropertyManagerException{
+    public PropertyNotLoadedException(String message) {
+        super(message);
+    }
+}
