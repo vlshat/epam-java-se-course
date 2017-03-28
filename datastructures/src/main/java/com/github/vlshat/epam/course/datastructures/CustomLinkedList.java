6e6d55570bbcb3de576e0a221aa9fa2afa9f00db
Vladislav Shatilenko <shatilenko.v@gmail.com>
2017-03-28 19:02:32 +0300

    linkedList add() contains() isEmpty() methods

--- datastructures/src/main/java/com/github/vlshat/epam/course/datastructures/CustomLinkedList.java
+++ datastructures/src/main/java/com/github/vlshat/epam/course/datastructures/CustomLinkedList.java
@@ -1,7 +1,160 @@
 package com.github.vlshat.epam.course.datastructures;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * Created by vladislav on 28.03.17.
  */
-public class CustomLinkedList {
+public class CustomLinkedList<T> implements List<T> {
+
+    private Node<T> head = new Node<>(null);
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head.hasNext();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+
+        Node<T> node = head;
+
+        while (node.hasNext()) {
+            node = node.next;
+            if (node.value.equals(o)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        Node<T> iterator = head;
+
+        while (iterator.hasNext()){
+            iterator = iterator.next;
+        }
+
+        iterator = new Node<>(t);
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    private class Node<T> {
+
+        private Node<T> next;
+        private T value;
+
+        Node(T value) {
+            this.value = value;
+        }
+
+        public boolean hasNext() {
+            return next != null;
+        }
+    }
 }
