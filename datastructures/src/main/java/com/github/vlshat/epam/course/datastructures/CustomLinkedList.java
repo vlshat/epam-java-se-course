803d0bc505451f585c926598e50667ff49c6f05b
Vladislav Shatilenko <shatilenko.v@gmail.com>
2017-03-28 18:14:48 +0300

    LinkedList init

---
+++ datastructures/src/main/java/com/github/vlshat/epam/course/datastructures/CustomLinkedList.java
@@ -0,0 +1,7 @@
+package com.github.vlshat.epam.course.datastructures;
+
+/**
+ * Created by vladislav on 28.03.17.
+ */
+public class CustomLinkedList {
+}
---
+++ datastructures/src/test/java/com/github/vlshat/epam/course/datastructures/CustomHashMapTest.java
@@ -0,0 +1,102 @@
+package com.github.vlshat.epam.course.datastructures;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+
+/**
+ * Created by vladislav on 27.03.17.
+ */
+@FixMethodOrder
+public class CustomHashMapTest {
+
+    private Map<Integer, String> m;
+
+    @Before
+    public void init() {
+        m = new CustomHashMap<>();
+    }
+
+    @Test
+    public void testThatWeCanCreate() {
+
+        assertThat(m, is(notNullValue()));
+
+    }
+
+
+
+    @Test
+    public void testThatNewMapIsEmpty() {
+        assertThat(m.isEmpty(), is(true));
+    }
+
+    @Test
+    public void testThatOnNewMapContainKeyMethodReturnFalseForAnyObject() {
+        assertThat(m.containsKey(new Integer(1)), is(false));
+    }
+
+    @Test
+    public void testThatWeCanPutKeyValuePairAndCanCheckIt() {
+        m.put(new Integer(3), "abc");
+        assertThat(m.containsKey(3), is(true));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThatWeCantPutNullKey() {
+        m.put(null, "abc");
+    }
+
+    @Test
+    public void testThatWeCanPutNullValue() {
+    }
+
+    @Test(expected = OutOfMemoryError.class)
+    public void testThatMapHaveInfiniteCapacity() {
+    }
+
+    @Test
+    public void testThatMapCanPutPairWithKeyThatAlreadyPresented() {
+    }
+
+    @Test
+    public void testThatMapCanContainsKeysWithSameHashCode() {
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThatContainsKeyMethodThrowsExceptionOnNullKey() {
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void testThatContainsKeyMethodThrowsExceptionOnWrongKeyClass() {
+    }
+
+    @Test
+    public void testContainsValueMethodWorksProperlyOn() {
+    }
+
+    @Test
+    public void testContainsValueMethodWorksProperlyOnNullInputValue() {
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void testValueContainsMethodThrowsExceptionOnWrongInputValueClass() {
+    }
+
+    @Test
+    public void testThatMapCalculateItsSizeProperly() {
+    }
+
+
+}
